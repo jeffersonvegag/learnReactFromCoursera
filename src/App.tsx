e3a2861c634fcbb018bad355f41bf746330358d2
Jefferson Vega <jefferson.vegag@gmail.com>
2025-05-16 10:32:26 -0500

    reestructuracion

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Saludar from './components/Saludo'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Saludar nombre="asdasdas"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Saludo.tsx
@@ -0,0 +1,2 @@
+const Saludar =({nombre="asdasd"}:{nombre?:string})=><h1>Hola {nombre}</h1>
+export default Saludar
